819b4661f7363c1de27eddf7085a0f3ad4a24b8f
amir12-python <amir.afkhami2015@gmail.com>
2026-09-11 09:08:40 +0330

    interpreter

---
+++ interpreter.py
@@ -0,0 +1,17 @@
+nm1,op,num2=input("Expression:").split("")
+num1=Float(num1)
+num2=Float(num2)
+
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1 - num2)      
+elif op == "*":
+    print(num1 * num2) 
+elif op == "/":
+    print(num1 / num2) 
+
+
+
+
+
