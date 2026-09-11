f977e0405d6acbb3041d0af88c84163bc6469391
amir12-python <amir.afkhami2015@gmail.com>
2026-09-11 08:18:53 +0330

    extens

---
+++ extensions.py
@@ -0,0 +1,35 @@
+user_file=input("file name:").lower().strip()
+if user_file.endswith(".gif"):
+    print("image/gif")
+elif user_file.endswith(".jpeg") or user_file.endswith(".jpg"):
+    print("image/jpeg")
+elif user_file.endswith(".txt"):
+    print("text/plain")
+elif user_file.endswith(".zip") or user_file.endswith(".jpg"):
+    print("application/zip")
+elif user_file.endswith(".pdf"):
+    print("application/pdf")
+elif user_file.endswith(".png"):
+    print("image/png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
